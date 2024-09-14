4215780e864261840feeea3b06170999c45139af
CosmeNicolas <nicomas7590@gmail.com>
2024-09-13 21:37:36 -0300

    agregue el controlador de mostrar usuarios y generar el id con crypto.random

--- controllers/usuarios.controllers.js
+++ controllers/usuarios.controllers.js
@@ -1,44 +1,49 @@
-
 const usuarios = [
   {
-    id:1,
-    nombreUsuario:"Nico2024",
-    emailDelUsuario:'nicoUsuario@gmail.com',
-    constrasenia : '123456789'
-  }
-]
+    nombreUsuario: "Nico2024",
+    emailDelUsuario: "nicoUsuario@gmail.com",
+    constrasenia: "123456789",
+  },
+];
 
 /* crear usuario */
-const crearUsuario = (req, res)=>{
+const crearUsuario = (req, res) => {
   try {
-    const body = req.body
-    const emailExiste = usuarios.find((usuario)=>usuario.emailDelUsuario === body.emailDelUsuario)
-    const usuarioExiste = usuarios.find((usuario)=> usuario.nombreUsuario === body.nombreUsuario)
+    const body = req.body;
+    const emailExiste = usuarios.find(
+      (usuario) => usuario.emailDelUsuario === body.emailDelUsuario
+    );
+    const usuarioExiste = usuarios.find(
+      (usuario) => usuario.nombreUsuario === body.nombreUsuario
+    );
     if (usuarioExiste) {
-    return res.status(400).json({msg:'Usuario no disponible'})
+      return res.status(400).json({ msg: "Usuario no disponible" });
     }
     if (emailExiste) {
-      return res.status(400).json({msg:'correo no disponible'})
+      return res.status(400).json({ msg: "correo no disponible" });
     }
-    usuarios.push(body)
-    res.status(201).json({msg:'Usuario Creado'})
+    const id = crypto.randomUUID();
+    usuarios.push({ id, ...body });
+    /* ... el spread copia el objeto y le agregamos el id */
+    res.status(201).json({ msg: "Usuario Creado" });
   } catch (error) {
-    res.status(500).json({ msg: "No se pudo crear el usuario", error });
+    console.log(error);
+    res.send("no se pudo crear el usuario");
   }
-}
+};
 
 /* Traer Usuarios */
-const mostrarUsuarios = async (req, res)=>{
+const mostrarUsuarios = async (req, res) => {
   try {
-    res.status(200).json({msg:'usuarios encontrados',usuarios})
+    res.status(200).json({ msg: "usuarios encontrados", usuarios });
   } catch (error) {
-    console.log(error)
-    res.status(500).json({msg:'Problema al traer los usuarios'})
+    console.log(error);
+    res.status(500).json({ msg: "Problema al traer los usuarios" });
   }
-}
+};
 
 /* exportar los modulos  */
 module.exports = {
   crearUsuario,
-  mostrarUsuarios
-}
+  mostrarUsuarios,
+};
